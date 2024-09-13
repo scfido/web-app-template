a50b662ee783335fbfa710cf5e47e791e604041b
fuyun <scfido@gmail.com>
2024-09-13 11:28:57 +0800

    增加头像预览

--- src/components/forms/AvatarFormItem.tsx
+++ src/components/forms/AvatarFormItem.tsx
@@ -1,14 +1,42 @@
 import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form"
 import { Input as ShadcnInput } from "@/components/ui/input"
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 import { cn } from "@/lib/utils"
 import { useFormContext } from "react-hook-form"
 import { useZodFormContext } from "@/components/forms/ZodForm"
 import { IFormItemProps } from "./types"
 import Help from "./_Help"
-import { DragEventHandler, useEffect, useState } from "react"
+import { DragEventHandler, useEffect, useRef, useState } from "react"
 import { Upload } from "lucide-react"
+import 'react-photo-view/dist/react-photo-view.css';
 
-export interface IAvatarFormItemProps extends IFormItemProps {
+interface IImagePreviewProps {
+    src?: string
+    onUpload?: () => void
+}
+
+const ImagePreview = ({ src, onUpload }: IImagePreviewProps) => {
+
+    const handleClick = (e: React.MouseEvent<SVGSVGElement>) => {
+        e.preventDefault()
+        onUpload?.()
+    }
+
+    return (
+        <PhotoProvider>
+            <div onClick={e => e.preventDefault()}>
+                {src ?
+                    <PhotoView src={src}>
+                        <img className="p-0.5 w-16 h-16 rounded-md bg-background" src={src} />
+                    </PhotoView>
+                    : <Upload className="w-8 h-8 text-muted-foreground cursor-pointer" onClick={handleClick} />
+                }
+            </div>
+        </PhotoProvider>
+    )
+}
+
+export interface IAvatarFormItemProps extends Omit<IFormItemProps, "placeholder"> {
     defaultValue?: string
     allowedFileTypes?: string[] // 允许的文件类型
 }
@@ -24,7 +52,6 @@ const AvatarFormItem = ({
     description,
     defaultValue,
     allowedFileTypes = ["jpg", "jpeg", "png", "svg"],
-    placeholder = "上传或拖动文件到这里",
     help,
     disabled = false,
 }: IAvatarFormItemProps) => {
@@ -36,7 +63,7 @@ const AvatarFormItem = ({
     const [accepted, setAccepted] = useState(false);
 
     useEffect(() => {
-        const { unsubscribe } = watch((formData, { name: triggerName, type }) => {
+        const { unsubscribe } = watch((formData, { name: triggerName }) => {
             if (triggerName === name) {
                 const file = formData[name]
 
@@ -120,6 +147,20 @@ const AvatarFormItem = ({
         }
     };
 
+    const uploadRef = useRef<HTMLInputElement>(null);
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (disabled)
+            return
+
+        if (e.key === "Enter" || e.key === " ") {
+            uploadRef.current?.click()
+        }
+    }
+
+    const handleUpload = () => {
+        uploadRef.current?.click()
+    }
+
     return (
         <FormField
             control={control}
@@ -139,34 +180,34 @@ const AvatarFormItem = ({
                                         "")}
                                     tabIndex={0}
                                     onDragEnter={handleDragIn}
+                                    onDragLeave={handleDragOut}
+                                    onDragOver={handleDragOver}
+                                    onDrop={handleDrop}
+                                    onKeyDown={handleKeyDown}
                                 >
                                     <div className={cn("flex items-center justify-center w-16 h-16 ")}>
-
-                                        {(image ?? defaultValue) ? <img className="p-0.5 w-16 h-16 rounded-md" src={image ?? defaultValue} />
-                                            : <Upload className="w-8 h-8 text-muted-foreground" />
-                                        }
+                                        <ImagePreview src={image ?? defaultValue} onUpload={handleUpload} />
                                     </div>
                                     <p className={cn("flex flex-col gap-1 text-foreground font-normal", { "invisible": dragging })}>
-                                        <span className="text-sm ">{placeholder}</span>
-                                        <span className="text-xs text-muted-foreground">接受<code className="bg-gray-100 text-gray-400 px-1 py-0.5 rounded">{allowedFileTypes?.join(", ")}</code>文件</span>
+                                        <span className="text-sm ">上传或拖动文件到这里</span>
+                                        <span className="text-xs text-muted-foreground">接受<code className="bg-muted text-muted-foreground px-1 py-0.5 rounded">{allowedFileTypes?.join(", ")}</code>文件</span>
                                     </p>
                                     <div
                                         className={cn(
-                                            "justify-center items-center absolute text-sm ",
+                                            "justify-center items-center absolute text-sm pointer-events-none",
                                             "-inset-0 border border-dashed bg-opacity-90 rounded",
                                             dragging ? "flex" : "hidden",
                                             accepted ? "border-primary bg-primary/30" : "border-red-600 bg-red-100",
                                         )}
-                                        onDragLeave={handleDragOut}
-                                        onDragOver={handleDragOver}
-                                        onDrop={handleDrop}
                                     >
-                                        <div className="pointer-events-none">{accepted ? "请释放鼠标" : "只支持图片文件"}</div>
+                                        <div className="pointer-events-none text-foreground">{accepted ? "请释放鼠标" : "只支持图片文件"}</div>
                                     </div>
                                 </div>
                             </FormLabel>
                             <FormControl>
-                                <ShadcnInput type="file"
+                                <ShadcnInput
+                                    ref={uploadRef}
+                                    type="file"
                                     className="sr-only"
                                     disabled={disabled}
                                     onBlur={onBlur}
